8c7935f27f9695c8f2eaba588e3e5fbd4cf1b13e
Gaurav Sirdeshpande <gsirdesh@nd.edu>
2017-05-01 21:29:43 -0400

    Resized all images, but we might have to make the board smaller to fit on screen, which means we'll have to change the sizes of images again

--- images.py
+++ images.py
@@ -3,16 +3,16 @@ import pygame
 pieceimages = {}
 
 # Load images
-pieceimages["WKing"] = pygame.image.load("images/wking.png")
-pieceimages["WQueen"] = pygame.image.load("images/wqueen.png")
-pieceimages["WBishop"] = pygame.image.load("images/wbishop.png")
-pieceimages["WRook"] = pygame.image.load("images/wrook.png")
-pieceimages["WKnight"] = pygame.image.load("images/wknight.png")
-pieceimages["WPawn"] = pygame.image.load("images/wpawn.png")
+pieceimages["WKing"] = pygame.image.load("small/wking.png")
+pieceimages["WQueen"] = pygame.image.load("small/wqueen.png")
+pieceimages["WBishop"] = pygame.image.load("small/wbishop.png")
+pieceimages["WRook"] = pygame.image.load("small/wrook.png")
+pieceimages["WKnight"] = pygame.image.load("small/wknight.png")
+pieceimages["WPawn"] = pygame.image.load("small/wpawn.png")
 
-pieceimages["BKing"] = pygame.image.load("images/bking.png")
-pieceimages["BQueen"] = pygame.image.load("images/bqueen.png")
-pieceimages["BBishop"] = pygame.image.load("images/bbishop.png")
+pieceimages["BKing"] = pygame.image.load("small/bking.png")
+pieceimages["BQueen"] = pygame.image.load("small/bqueen.png")
+pieceimages["BBishop"] = pygame.image.load("small/bbishop.png")
 pieceimages["BRook"] = pygame.image.load("small/brook.png")
 pieceimages["BKnight"] = pygame.image.load("small/bknight.png")
-pieceimages["BPawn"] = pygame.image.load("images/bpawn.png")
+pieceimages["BPawn"] = pygame.image.load("small/bpawn.png")
